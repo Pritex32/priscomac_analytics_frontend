4ba7723a1080e0a7ff23efe500f2ed331dc2226d
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2026-08-09 04:01:59 +0100

    Add GetLicense page and update LandingPage props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LandingPage from './pages/LandingPage'
 import AnalysisPage from './pages/AnalysisPage'
 import ResultsPage from './pages/ResultsPage'
+import GetLicense from './pages/GetLicense'
 import { useSession } from './utils/api'
 
 function App() {
   const [page, setPage] = useState('landing')
   const [analysisData, setAnalysisData] = useState(null)
   const { session, setSessionToken, clearSession } = useSession()
+
+  useEffect(() => {
+    if (window.location.pathname === '/get-license') {
+      setPage('get-license')
+    }
+  }, [])
 
   const handleLicenseVerified = (data) => {
     setSessionToken(data.token)
@@ -31,13 +38,16 @@ function App() {
   return (
     <div className="min-h-screen bg-white">
       {page === 'landing' && (
-        <LandingPage onLicenseVerified={handleLicenseVerified} />
+        <LandingPage onLicenseVerified={handleLicenseVerified} onGetLicense={() => setPage('get-license')} />
       )}
       {page === 'analysis' && (
         <AnalysisPage onAnalysisComplete={handleAnalysisComplete} token={session} />
       )}
       {page === 'results' && analysisData && (
         <ResultsPage data={analysisData} onRunAnother={handleRunAnother} />
+      )}
+      {page === 'get-license' && (
+        <GetLicense onBack={() => setPage('landing')} />
       )}
     </div>
   )
